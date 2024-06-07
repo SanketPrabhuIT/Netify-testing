751689b31110cca2a32888c097a8aa42c827073b
Sanket Prabhu <soum2022r@gmail.com>
2024-06-07 22:55:32 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'feedback',
+    loadChildren: () => import('./feedback/feedback.module').then( m => m.FeedbackPageModule)
+  },
+  {
+    path: 'card1',
+    loadChildren: () => import('./card1/card1.module').then( m => m.Card1PageModule)
+  },
+  {
+    path: 'card2',
+    loadChildren: () => import('./card2/card2.module').then( m => m.Card2PageModule)
+  },
+  {
+    path: 'card3',
+    loadChildren: () => import('./card3/card3.module').then( m => m.Card3PageModule)
+  },
+  {
+    path: 'card4',
+    loadChildren: () => import('./card4/card4.module').then( m => m.Card4PageModule)
   }
 ];
 @NgModule({
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor() { }
 
+  event1() {
+    const eventLink = 'https://www.tradeindia.com/tradeshows/123802/mumbai-art-fair-2024.html';
+    window.open(eventLink, '_blank');
+  }
+  event2() {
+    const eventLink = 'https://forarthistory.org.uk/events/rcp-museum-taster-tour-history/';
+    window.open(eventLink, '_blank');
+  }
+  event3() {
+    const eventLink = 'https://forarthistory.org.uk/events/the-womens-art-collection-artworks-and-artists-in-context-murray-edwards-college/';
+    window.open(eventLink, '_blank');
+  }
+  event4() {
+    const eventLink = 'From Morris wallpapers, to Voysey chairs and De Morgan ceramics, the artists and designers of the late 19th century Arts and Crafts and Aesthetic movements created some of the most beautiful and iconic objects ever produced in Britain. Join Dr Emma...';
+    window.open(eventLink, '_blank');
+  }
 }
